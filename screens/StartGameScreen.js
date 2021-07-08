417d73deb4ae3c9b037470f23ada176a5d5162a4
Vikas Dubey <vikasdubeysu9090@gmail.com>
2021-07-08 19:56:01 +0530

    guess game

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, StyleSheet, Text, Button, TextInput } from "react-native";
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Lets Start the game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    elevation: 9,
+    padding:20,
+    borderRadius:10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+});
+
+export default StartGameScreen;
